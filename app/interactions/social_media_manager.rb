acdc205d2e7721d8d1573223c356dd890dcb0ea7
rakeshtech25 <RakeshSag2598@gmail.com>
2021-09-28 16:53:19 +0530

    Added services to simplify the interaction flow

--- app/interactions/social_media_manager.rb
+++ app/interactions/social_media_manager.rb
@@ -7,39 +7,11 @@ class SocialMediaManager < ActiveInteraction::Base
     threads = []
     SOCIAL_MEDIA_HASH.to_h.each do |platform, object|
       threads << Thread.new do
-        response_hash[platform] = get(platform)
+        response_hash[platform] = RemoteRequest.get(platform)
       end
     end
     threads.each(&:join)
 
     response_hash
   end
-
-  def get(platform)
-    url = "#{ENV['BASE_URL']}/#{platform}"
-    response = HTTParty.get(url)
-    object = SOCIAL_MEDIA_HASH[platform.to_sym]
-
-    case response.code
-    when 200
-      extract_data(response, object)
-    when 404
-      "Data not found!"
-    when 500...600
-      "ERROR #{response.code}"
-    else
-      "Something went wrong!"
-    end
-  end
-
-  private
-
-  def extract_data(response, object)
-    filtered_data = JSON.parse(response.body).map{|res| res[object] }.compact
-
-    return filtered_data if filtered_data.present?
-
-    "Response Error!"
-  end
-
 end
